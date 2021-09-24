ffd02d5aaddcf643763aa9baf9552519bc61b9a8
unknown <arimrimunanda15@gmail,com>
2021-09-24 15:25:59 +0700

    first commit

---
+++ rimunanda.java
@@ -0,0 +1,9 @@
+public class Rimunanda {
+    public static void main(String[] args) {
+        System.out.println("Nama		: Rimunanda Ma'arif Parinduri");
+		System.out.println("NIM		: 6706213105"); 
+		System.out.println("kelas		: RPLA 45 04");
+		
+    }
+
+}
